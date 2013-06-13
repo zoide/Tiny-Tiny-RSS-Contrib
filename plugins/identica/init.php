db4b92e16143ed15a5682488a791425dfffbf7ad
Heiko Adams <heiko.adams@gmail.com>
2013-06-13 19:03:57 +0200

    Make status.net instance configurable and remove the hardcoded URL

--- plugins/identica/init.php
+++ plugins/identica/init.php
@@ -6,12 +6,19 @@ class Identica extends Plugin {
 		$this->host = $host;
 
 		$host->add_hook($host::HOOK_ARTICLE_BUTTON, $this);
+		$host->add_hook($host::HOOK_PREFS_TAB, $this);
 	}
 
 	function about() {
 		return array(1.0,
-			"Share articles on Identi.ca",
+			"Share articles on status.net sites",
 			"fox");
+	}
+
+	function save() {
+		$status_url = db_escape_string($_POST["status_url"]);
+		$this->host->set($this, "status_url", $status_url);
+		echo "Value Status.net URL set to $status_url<br/>";
 	}
 
 	function get_js() {
@@ -42,8 +49,48 @@ class Identica extends Plugin {
 			$article_link = db_fetch_result($result, 0, 'link');
 		}
 
+		$status_url = $this->host->get($this, "status_url");
+
 		print json_encode(array("title" => $title, "link" => $article_link,
-				"id" => $id));
+				"id" => $id, "status_url" => $status_url));
+	}
+	
+	function hook_prefs_tab($args) {
+		if ($args != "prefPrefs") return;
+
+		print "<div dojoType=\"dijit.layout.AccordionPane\" title=\"".__("Status.net settings")."\">";
+
+		print "<br/>";
+
+		$status_url = $this->host->get($this, "status_url");
+		print "<form dojoType=\"dijit.form.Form\">";
+
+		print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+			evt.preventDefault();
+		if (this.validate()) {
+			console.log(dojo.objectToQuery(this.getValues()));
+			new Ajax.Request('backend.php', {
+parameters: dojo.objectToQuery(this.getValues()),
+onComplete: function(transport) {
+notify_info(transport.responseText);
+}
+});
+}
+</script>";
+
+print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"identica\">";
+print "<table width=\"100%\" class=\"prefPrefsList\">";
+print "<tr><td width=\"40%\">".__("Status.net URL")."</td>";
+print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" required=\"1\" name=\"status_url\" regExp='^(http|https)://.*' value=\"$status_url\"></td></tr>";
+	print "</table>";
+	print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
+
+	print "</form>";
+
+	print "</div>"; #pane
+
 	}
 
 	function api_version() {
